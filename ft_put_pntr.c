81ace14d669ccdd11df910976662100b6c879dbf
Luceva2002 <luceva2002@gmail.com>
2025-01-14 13:11:52 +0100

    ft_printf

--- ft_put_pntr.c
+++ ft_put_pntr.c
@@ -6,30 +6,37 @@
 /*   By: luevange <luevange@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/13 02:24:03 by luevange          #+#    #+#             */
-/*   Updated: 2025/01/13 11:42:32 by luevange         ###   ########.fr       */
+/*   Updated: 2025/01/14 13:11:19 by luevange         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
 
-int	ft_put_pntr(unsigned long ptr)
+int ft_ptr(unsigned long ptr, int cnt)
 {
-	char	*hex;
-	int		count;
+    char *set;
 
-	hex = "0123456789abcdef";
-	count = 0;
-	if (ptr == 0)
-	{
-		write(1, "(nil)", 5);
-		return (5);
-	}
-	if (count < 2)
-		write(1, "0x", 2);
-	count += 2;
-	if (ptr >= 16)
-		count += ft_put_pntr(ptr / 16);
-	write(1, &hex[ptr % 16], 1);
-	count++;
-	return (count);
+    set = "0123456789abcdef";
+    if (ptr >= 16)
+        cnt += ft_put_pntr(ptr / 16);
+    write(1, &set[ptr % 16], 1);
+    cnt ++;
+    return (cnt);
+}
+
+int ft_put_pntr(unsigned long ptr)
+{
+    int cnt;
+
+    cnt = 0;
+    if (ptr == 0)
+    {
+        write(1, "(nil)", 5);
+        return 5;
+    }
+    if (count < 2)
+        write(1, "0x", 2);
+    cnt += 2;
+    ft_ptr(ptr, cnt);
+    return (cnt);
 }
